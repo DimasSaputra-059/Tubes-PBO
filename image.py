a806513501ee33adf6a4a82b0e4885559a807e44
Rizki Esa Fadillah <126938786+Rizki-Esa-Fadillah@users.noreply.github.com>
2023-05-13 12:10:32 +0700

    Add files via upload

---
+++ image.py
@@ -0,0 +1,9 @@
+import pygame
+import os
+background=pygame.image.load(os.path.join("image","background.jpg"))
+bullet=pygame.image.load(os.path.join("image","bullet.png"))
+player=pygame.image.load(os.path.join("image","player.png"))
+rock=pygame.image.load(os.path.join("image","rock.png"))
+power=pygame.image.load(os.path.join("image","power.png"))
+boss=pygame.image.load(os.path.join("image","boss.png"))
+alien=pygame.image.load(os.path.join("image","alien.png"))
