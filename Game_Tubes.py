82e1df22a2a5b835d4d13669ea9e2fe7af4cfe1e
Rizki Esa Fadillah <126938786+Rizki-Esa-Fadillah@users.noreply.github.com>
2023-05-14 20:26:17 +0700

    Update Game_Tubes.py
    Udah aku kasih komentar, biar gampang memahami kodenya. 
    Kalo mau nambahin fitur lain boleh, misalnya petunjuk permainan

--- Game_Tubes.py
+++ Game_Tubes.py
@@ -22,6 +22,7 @@ fps = pygame.time.Clock()
 #Class Player(Class Child)
 class Player(pygame.sprite.Sprite):
     def __init__(self):
+        # Informasi dasar/awal seputar player
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.transform.scale(image.player,(80,75))
         self.rect = self.image.get_rect()
@@ -37,14 +38,14 @@ class Player(pygame.sprite.Sprite):
         self.shoot_speed = 0
 
     #artibuted movement
-    def update(self):
-        if self.power >= 2 and pygame.time.get_ticks() - self.power_time > 5000:
-            self.power -= 1
+    def update(self): # Fungsi untuk mengatur durasi palyer menerima bonus
+        if self.power >= 2 and pygame.time.get_ticks() - self.power_time > 5000: # durasi palyer menerima bonus power per satuan milidetik
+            self.power -= 1 
             self.power_time = pygame.time.get_ticks()
         
-        if self.shoot_speed >= 1 and pygame.time.get_ticks() - self.speed_time > 10000:
+        if self.shoot_speed >= 1 and pygame.time.get_ticks() - self.speed_time > 10000:# durasi palyer menerima speed power per satuan milidetik
             self.shoot_speed -= 1
-            self.shoot_delay += 250
+            self.shoot_delay += 250 # Mengembalikan delay shoot seperti semula ketika bonus berakhir
             self.speed_time = pygame.time.get_ticks()
             
         key_pressed = pygame.key.get_pressed()
@@ -66,28 +67,28 @@ class Player(pygame.sprite.Sprite):
         if self.rect.top < 0:
             self.rect.top  = 0
 
-    def powerup(self):
+    def powerup(self): # fungsi untuk mengatur manfaat bonus power
         self.power += 1
         self.power_time = pygame.time.get_ticks()
     
-    def heal(self):
-        self.life += 1
+    def heal(self): # fungsi untuk mengatur manfaat bonus heal
+        self.life += 1 # Menmbah nyawa player
     
-    def speed_shoot(self):
+    def speed_shoot(self): # fungsi untuk mengatur manfaat bonus speed
         self.shoot_speed += 1
-        self.shoot_delay -= 250
+        self.shoot_delay -= 250 # mempercepat delay saat menembak 
         self.speed_time = pygame.time.get_ticks()
 
-    def shoot(self):
+    def shoot(self): # fungsi untuk mengatur tembakan palyer
         now = pygame.time.get_ticks()
         if now - self.last_shot > self.shoot_delay:
             self.last_shot = now
-            if self.power == 1:
+            if self.power == 1: # Player akan menembakkan 1 bullet/peluru
                 bullet = Bullet(pygame.Vector2(self.rect.centerx,self.rect.top))
                 all_sprites.add(bullet)
                 bullets.add(bullet)
                 sound.missile.play()
-            elif self.power >= 2:
+            elif self.power >= 2: # Player akan menembakkan 2 bullet/peluru
                 bullet1 = Bullet(pygame.Vector2(self.rect.centerx-20,self.rect.top))
                 all_sprites.add(bullet1)
                 bullets.add(bullet1)
@@ -96,10 +97,10 @@ class Player(pygame.sprite.Sprite):
                 bullets.add(bullet2)
                 sound.missile.play()  
 
-    def show_lifepoints(self):
+    def show_lifepoints(self): # fungsi untuk menampilkan informasi score life point/ darah
         draw_text(layar, f"life points -> {self.life}", 15, WIDTH-70, HEIGHT-590)
 
-    def show_score(self):
+    def show_score(self): # Fungsi untuk menampilkan informasi score 
         draw_text(layar, f"Score -> {self.score_val}", 15, WIDTH-450, HEIGHT-590)
 
 # Implementasi Polymorphisme
@@ -142,6 +143,7 @@ class Bonus(abc.ABC, pygame.sprite.Sprite):
     def update(self):
         pass
 
+# Class Power (Class child dari pygame)
 class Power(Bonus):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -156,6 +158,7 @@ class Power(Bonus):
         if self.rect.top > HEIGHT:
             self.kill()
 
+# Class HealUP (Class child dari pygame)
 class HealUP(Bonus):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -170,6 +173,7 @@ class HealUP(Bonus):
         if self.rect.top > HEIGHT:
             self.kill()
 
+# Class SpeedShoot (Class child dari pygame)
 class SpeedShoot(Bonus):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -178,7 +182,6 @@ class SpeedShoot(Bonus):
         self.radius=self.rect.width*0.1/2
         self.rect.x=random.randrange(0,WIDTH-self.rect.width)
         self.speedy = 5
-
 
     def update(self):
         self.rect.y += self.speedy
@@ -213,6 +216,7 @@ class Healthbar(pygame.sprite.Sprite):
 #Class Boss(Class Child dari class pygame)
 class Boss(pygame.sprite.Sprite):
     def __init__(self, max_health:int, attack_speed:int = 50):
+        # Baris kode mengatur posisi boss saat muncul
         pygame.sprite.Sprite.__init__(self)
         self.source_image = pygame.transform.rotate(pygame.transform.scale(image.boss,(200,180)),90)
         self._angle = 180
@@ -221,6 +225,7 @@ class Boss(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH/2
         self.rect.bottom = 0
 
+        # Informasi dasar/awal seputar boss
         self.max_health = max_health
         self._health = 0
         self.healthbar = Healthbar()
@@ -233,44 +238,44 @@ class Boss(pygame.sprite.Sprite):
 
     #Konsep Enkapsulasi (Setter dan Getter)
     @property
-    def health(self):
+    def health(self): # fungsi getter health
         return self._health
 
     @health.setter
-    def health(self, value):
+    def health(self, value): # fungsi setter health, beserta gambar bar darah boss
         self._health = value
         self.healthbar.image.fill((255,0 , 0))
         self.healthbar.image.fill((0, 255, 0), (0, 0, self.healthbar.image.get_width()*self.health/self.max_health, self.healthbar.image.get_height()))
 
     @property
-    def angle(self):
+    def angle(self): # fungsi getter angel/sudut
         return self._angle
 
     @angle.setter
-    def angle(self, value):
+    def angle(self, value): # fungsi getter angle/sudut
         if value != self._angle:
             self._angle = value
             self.on_angle_change()
 
-    def on_angle_change(self):
+    def on_angle_change(self): # fungsi untuk mengatur gerak boss ketika menembak player
         self.image = pygame.transform.rotate(self.source_image, self.angle)
 
-    def hurt(self, value:int = 10):
+    def hurt(self, value:int = 10): # fungsi untuk mengatur kalkulasi bar darah boss
         self.health -= value
         if self.health <= 0:
             self.kill()
             self.healthbar.kill()
  
-    def shoot(self):
+    def shoot(self): # fungsi untuk mengatur sudut  rotasi tembakan boss
         self.alt = not self.alt
         if self.alt:
-            bullet = Bullet(pygame.Vector2(self.rect.centerx-30,self.rect.bottom), -self.angle)
+            bullet = Bullet(pygame.Vector2(self.rect.centerx-30,self.rect.bottom), -self.angle) 
         else :
             bullet = Bullet(pygame.Vector2(self.rect.centerx+30,self.rect.bottom), -self.angle)
         all_sprites.add(bullet)
         hazard.add(bullet)
 
-    def update(self):
+    def update(self): # fungsi agar boss dapat menembak player secara otomatis
         self.rect.y += self.move_in.y
         if self.move_in.y > 0:
             self.move_in.y *= 0.95
@@ -280,66 +285,67 @@ class Boss(pygame.sprite.Sprite):
         s_center = self.rect.center
         angle_in_rads = math.atan2(p_center[1] - s_center[1], p_center[0] - s_center[0])
 
-        self.angle = -math.degrees(angle_in_rads)
+        self.angle = -math.degrees(angle_in_rads) 
         if self.tick > self.attack_speed:
             self.tick = 0
             self.shoot()
 
-    def kill(self):
+    def kill(self): # Fungsi ketika boss dikalahkan
         self.healthbar.kill()
         return super().kill()
         
-#Tampilan kedua setelah menu()
+#Fungsi tampilan kedua setelah menu() / Tampilan waiting
 def waiting_screen():
     layar.blit(pygame.transform.scale(image.background,(500,700)),(0,0))
     draw_text(layar, "GALACTIC WAR!", 40, WIDTH/2, HEIGHT/4)
     draw_text(layar, "Arrow keys to move, Space key to fire", 20, WIDTH/2, HEIGHT/2)
     draw_text(layar, "Press any key to play", 22, WIDTH/2, HEIGHT*3/4)
-    pygame.display.flip()
-    waiting = True
-    while waiting:
-        fps.tick(FPS)
+    pygame.display.flip() # Memperbarui tampilan setelah di update
+
+    waiting = True # kondisi awal
+    while waiting: # menu waiting berjalan
+        fps.tick(FPS) #Menagtur kecepatan fps
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # game akan berakhir jika pada menu game over, user memilih quit
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.KEYUP:
+            if event.type == pygame.KEYUP: # Menu waiting akan berakhir jika user mengklik tombol keyboard (manapun) 
                 waiting = False
 
-#Tampilan awal
+#Fungsi tampilan Menu game
 def menu():
     layar.blit(pygame.transform.scale(image.background,(500,700)),(0,0))
     draw_text(layar, "GALACTIC WAR!", 50, WIDTH/2, HEIGHT/4)    
-    pygame.display.flip()
+    pygame.display.flip() # Memperbarui tampilan setelah di update
     yvar=325
     xvar=250
     draw_text(layar, "START", 35, WIDTH/2, yvar-15) 
     draw_text(layar, "QUIT",40, WIDTH/2, 500)  
     pygame.draw.circle(layar, (GREEN), (xvar,yvar), 70,6)
     
-    waiting = True
-    while waiting:
-        fps.tick(FPS)
+    waiting = True # Kondisi awal
+    while waiting: # Layar menu berjalan
+        fps.tick(FPS) #Mengatur kecepatan fps
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # game akan berakhir jika pada menu game over, user memilih quit
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: # Jika tombol mouse ditekan, kode akan diajalnkan
                 xpos, ypos = pygame.mouse.get_pos()
                 cek = math.sqrt((xvar - xpos)**2 + (yvar - ypos)**2)
-                if cek <= 70:
-                    waiting = False
-                    waiting_screen()
+                if cek <= 70: # Titik koordinat mouse, memulai game 
+                    waiting = False # layar menu berhenti
+                    waiting_screen() # layar waiting berjalan
                     return False
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: # Jika tombol mouse ditekan, kode akan diajalnkan
                 xpos, ypos = pygame.mouse.get_pos()
                 cek = math.sqrt((xvar - xpos)**2 + (520 - ypos)**2)
-                if cek <= 25:
+                if cek <= 25: # Titik koordinat mouse,  game berakhir 
                     pygame.quit()
                     sys.exit()
-        pygame.display.update()
+        pygame.display.update() # Tampilan layar game akan di update sesuai perintah
 
-#Tampilan ketika GameOver
+#Fungsi tampilan menu ketika GameOver
 def menuGameOver():
     layar.blit(pygame.transform.scale(image.background,(500,700)),(0,0))
     draw_text(layar, "Game Over", 50, WIDTH/2, HEIGHT/4)   
@@ -350,37 +356,41 @@ def menuGameOver():
     draw_text(layar, "QUIT",40, WIDTH/2, 500) 
     pygame.draw.circle(layar, (GREEN), (xvar,yvar), 70,6)
     
-    waiting = True
-    while waiting:
-        fps.tick(FPS)
+    waiting = True # Kondisi awal
+    while waiting: # Layar game over berjalan
+        fps.tick(FPS) # Mengatur kecepatan fps
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: # game akan berakhir jika pada menu game over, user memilih quit
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: # Jika tombol mouse ditekan, kode akan diajalnkan
                 xpos, ypos = pygame.mouse.get_pos()
                 cek = math.sqrt((xvar - xpos)**2 + (yvar - ypos)**2)
-                if cek <= 70:
-                    player.score_val = 0
-                    waiting = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
+                if cek <= 70: # Titik koordinat mouse, memulai game
+                    player.score_val = 0 
+                    waiting = False # Layar game over berhenti
+            if event.type == pygame.MOUSEBUTTONDOWN: # Jika tombol mouse ditekan, kode akan diajalnkan
                 xpos, ypos = pygame.mouse.get_pos()
                 cek = math.sqrt((xvar - xpos)**2 + (520 - ypos)**2)
-                if cek <= 25:
+                if cek <= 25: # Titik koordinat mouse, game berhenti/ditutup
                     pygame.quit()
                     sys.exit()
-        pygame.display.update()
+        pygame.display.update() # Tampilan layar game akan di update sesuai perintah
 
+# Kondisi ketika game over terpenuhi
 game_over = True
-running=True
-menu()
+running=True # kondisi awal
 hp = 0
-sound.bgmusic.play(loops=-1)        
-while running:
-    fps.tick(FPS)
-    # waiting screen ketika gameover dan akan memulai game
+
+# Tampilan awal menu
+menu() 
+sound.bgmusic.play(loops=-1) # Musik akan berputar tanpa henti (karena loops = -1)
+while running: # layar menu berjalan
+    fps.tick(FPS) #Menagtur kecepatan fps
+    # waiting screen ketika gameover dan akan memulai game, game akan di reset untuk kembali seperti semula 
     if game_over:
         game_over = False
+        # Kondisi awal game
         all_sprites = pygame.sprite.Group()
         hazard = pygame.sprite.Group()
         bullets = pygame.sprite.Group()
@@ -388,6 +398,7 @@ while running:
         level = 1
         all_sprites.add(player)
 
+        # 3 Alien akan muncul setiap awal game
         for i in range(3):
             alien=Alien()
             all_sprites.add(alien)
@@ -395,17 +406,16 @@ while running:
         player.score_val = 0
 
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            running=False
+        if event.type==pygame.QUIT: # game akan berakhir jika pada menu game over, user memilih quit
+            running=False # layar menu berhenti
             pygame.quit()
             sys.exit()
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_SPACE: # keyboard spasi untuk menembak
                 player.shoot()
+    all_sprites.update() # semua objek di update.
 
-    all_sprites.update()
     hits=pygame.sprite.groupcollide(hazard,bullets,False,True)
-
     for hit in hits:
         # mengecek apakah peluru mengenai alien, jika kena maka alien akan kembali respawn
         if isinstance(hit, Alien):
@@ -450,6 +460,7 @@ while running:
                 all_sprites.add(rock)
                 hazard.add(rock)
 
+        # cek apakah peluru mengenai Rock        
         elif isinstance(hit, Rock):
             sound.exlp2.play()
             hit.kill()
@@ -465,6 +476,7 @@ while running:
         alien.speedx=random.randrange(-3,1)
         alien.speedy=random.randrange(3,7)
         
+    # Tampilan layar dalam permainan
     layar.blit(pygame.transform.scale(image.background,(500,700)),(0,0))
     draw_text(layar, f"Level {level}", 24, WIDTH/2, HEIGHT-590)
     all_sprites.draw(layar)
@@ -473,8 +485,8 @@ while running:
     pygame.display.update()
 
     hits = pygame.sprite.spritecollide(player,hazard,False,pygame.sprite.collide_circle)
-    # jika pesawat terkena hit, life akan berkurang
     for hit in hits:
+        # jika pesawat terkena hit Alien, life akan berkurang, dan alien yang hancur akan respawn
         if isinstance(hit, Alien):
             sound.expl.play()
             hit.kill()
@@ -483,32 +495,40 @@ while running:
             hazard.add(alien)
             player.life -= 1
 
+        # jika pesawat terkena hit Rock (Batu), life akan berkurang
         elif isinstance(hit, Rock):
             sound.expl.play()
             hit.kill()
             player.life -= 1
 
+        # jika pesawat terkena hit Bullet (Peluru) dari boss, life akan berkurang
         elif isinstance(hit, Bullet):
             sound.expl.play()
             hit.kill()
             player.life -= 1
 
+        # jika pesawat mengambil bonus power, player akan memperoleh manfaat bonus
         elif isinstance(hit, Power):
             hit.kill()
             sound.powerup.play()
             player.powerup()
 
+        # jika pesawat mengambil bonus SpeedShoot, player akan memperoleh manfaat bonus
         elif isinstance(hit, SpeedShoot):
             hit.kill()
             sound.speedup.play()
             player.speed_shoot()
+
+        # jika pesawat mengambil bonus heal, player akan memperoleh manfaat bonus
         else :
             hit.kill()
             sound.heal.play()
             player.heal()
-             
+        
+        # Jika life (Nyawa) player habis, game akan berakhir dan menampilkan menu game over
         if player.life < 0:
             game_over = True
             menuGameOver()     
-        
+
+# Game berakhir   
 pygame.quit()
